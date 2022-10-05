0b0208c0c9eb1c95477f7417197f73e93cc07e4d
muditvir <mvirmani02@gmail.com>
2022-10-05 10:48:40 +0530

    v2

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -55,10 +55,10 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('row-pivots','["timestamp"]');
       elem.setAttribute('columns','["top_ask_price"]');
       elem.setAttribute('aggregates',
-      {"stock":"distinct count",
+      '{"stock":"distinct count",
       "top_ask_price":"avg",
       "top_bid_price":"avg",
-      "timestamp":"distinct count"});
+      "timestamp":"distinct count"}');
     }
   }
 
